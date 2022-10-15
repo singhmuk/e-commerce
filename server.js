d0629746aabcc2f3be1dbf0578810ff14cad8aaf
mukesh <mukese0794@gmail.com>
2022-10-15 15:58:47 +0530

    Placed order

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 import seedRouter from "./routes/seedRoutes.js";
 import ProductRouter from "./routes/productRoutes.js";
 import userRouter from "./routes/userRoutes.js";
+import orderRouter from './routes/orderRoutes.js';
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/seed', seedRouter);
 app.use('/api/products', ProductRouter);
 app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
