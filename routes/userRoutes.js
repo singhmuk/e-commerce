d4a2aba44e74df9fe1f9ca25e7a2aec1fc336ac5
mukesh <mukese0794@gmail.com>
2022-10-16 00:00:00 +0530

    edit user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -52,6 +52,31 @@ userRouter.get('/', isAuth, isAdmin, expressAsyncHandler(async (req, res) => {
 );
 
 
+userRouter.get('/:id', isAuth, isAdmin, expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      res.send(user);
+    } else {
+      res.status(404).send({ message: 'User Not Found' });
+    }
+  })
+);
+
+userRouter.put('/:id', isAuth, isAdmin, expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      user.isAdmin = Boolean(req.body.isAdmin);
+      const updatedUser = await user.save();
+      res.send({ message: 'User Updated', user: updatedUser });
+    } else {
+      res.status(404).send({ message: 'User Not Found' });
+    }
+  })
+);
+
+
 userRouter.put('/profile', isAuth, expressAsyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id);
     if (user) {
