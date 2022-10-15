f68b06af211d28c6fb30d9373e8a283b453601f3
mukesh <mukese0794@gmail.com>
2022-10-16 00:23:16 +0530

    review orders

--- models/productModel.js
+++ models/productModel.js
@@ -1,7 +1,15 @@
 import mongoose from 'mongoose';
 
-const productSchema = new mongoose.Schema(
-  {
+
+const reviewSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  comment: { type: String, required: true },
+  rating: { type: Number, required: true },
+},
+{timestamps: true}
+);
+
+const productSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
     slug: { type: String, required: true, unique: true },
     image: { type: String, required: true },
@@ -12,11 +20,13 @@ const productSchema = new mongoose.Schema(
     countInStock: { type: Number, required: true },
     rating: { type: Number, required: true },
     numReviews: { type: Number, required: true },
+
+    //Review-Orders
+    reviews: [reviewSchema]
   },
-  {
-    timestamps: true,
-  }
+  {timestamps: true}
 );
+
 
 const Product = mongoose.model('Product', productSchema);
 export default Product;
