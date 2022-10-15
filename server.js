73e0213d23198b70b46124be2eb6560ad1826a4b
mukesh <mukese0794@gmail.com>
2022-10-15 13:14:40 +0530

    connect db

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 import express from "express";
 const app = express();
 
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import data from "./data.js"
 
-app.get('/api/products', (req,res)=>{
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL, () => {
+    console.log('Connected to DB')
+}).catch(err => {
+    console.log(err)
+})
+
+app.get('/api/products', (req,res) => {
     res.send(data.products)
 });
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
