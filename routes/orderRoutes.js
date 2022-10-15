9785198d0751c6cbc5fa4f733d849f9eb7369ee6
mukesh <mukese0794@gmail.com>
2022-10-15 22:18:09 +0530

    order history

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -34,6 +34,13 @@ orderRouter.get('/', isAuth, expressAsyncHandler(async (req, res) => {
   })
 );
 
+//Display-Order-History
+orderRouter.get('/mine', isAuth, expressAsyncHandler(async (req, res) => {
+    const orders = await Order.find({ user: req.user._id });
+    res.send(orders);
+  })
+);
+
 orderRouter.get('/:id', isAuth, expressAsyncHandler(async (req, res) => {
   const order = await Order.findById(req.params.id);
   if (order) {
